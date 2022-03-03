e67e2a9689ef336915a1d55b892d81f0dd0bb33b
Benny Hernandez <mail@bennyhernandez.com>
2022-03-03 13:09:20 -0500

    export ui components

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,7 @@
+import FlexContainer from "./FlexContainer";
+import Heading from "./Heading";
+import Intro from "./Intro";
+import SubHeading from "./SubHeading";
+import Text from "./Text";
+import Button from "./Button";
+export { FlexContainer, Heading, Intro, SubHeading, Text, Button };
