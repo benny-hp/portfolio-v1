164cfe6bab6e594b52e4e43b7e37bfae53f179ff
Benny Hernandez <mail@bennyhernandez.com>
2022-03-03 13:05:40 -0500

    feature: create heading ui component

---
+++ src/components/ui/Heading.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { css, useTheme } from "@emotion/react";
+interface Props {
+  children: React.ReactNode;
+  mb: 0 | 1 | 2 | 3 | 4 | 5;
+  weight?: "bold" | "bolder" | "boldest";
+  color?:
+    | "primary"
+    | "primaryLight"
+    | "primaryDark"
+    | "secondary"
+    | "light"
+    | "dark"
+    | "neutral";
+}
+const Heading = ({ children, mb, weight = "boldest", color }: Props) => {
+  const theme = useTheme();
+  return (
+    <h2
+      css={css`
+        margin-bottom: ${theme.space[mb]}rem;
+        font-size: ${theme.fontSizes.heading}rem;
+        font-weight: ${theme.fontWeights[weight]};
+        ${color && `color: ${theme.colors[color]}`}
+      `}
+    >
+      {children}
+    </h2>
+  );
+};
+
+export default Heading;
