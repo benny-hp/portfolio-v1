c7d43bd196b92270ac428c4fda183e2456d214fd
Benny Hernandez <mail@bennyhernandez.com>
2022-03-03 13:08:26 -0500

    Feature: create button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { css, ThemeContext, useTheme } from "@emotion/react";
+import Link from "next/link";
+
+interface Props {
+  children: React.ReactNode;
+  mt?: 0 | 1 | 2 | 3 | 4 | 5;
+  href?: string;
+  variant?: "outline";
+  ml?: 0 | 1 | 2 | 3 | 4 | 5;
+  cta?: boolean;
+  type?: "submit";
+}
+const Button = ({
+  children,
+  mt = 3,
+  href,
+  variant,
+  ml = 0,
+  cta,
+  type,
+}: Props) => {
+  const theme = useTheme();
+  const linkButtonStyles = css`
+    padding: ${theme.space[1]}rem ${theme.space[4]}rem;
+
+    margin-left: ${theme.space[ml]}rem;
+    font-size: ${theme.fontSizes.normal}rem;
+    background-color: ${variant === "outline"
+      ? "transparent"
+      : theme.colors.primary};
+    border: solid ${theme.colors.primary} 2px;
+    border-radius: 0.4rem;
+    display: inline-block;
+    margin-top: ${theme.space[mt]}rem;
+    color: ${theme.colors.light};
+    color: ${variant === "outline" ? theme.colors.primary : theme.colors.light};
+    transition: background-color 0.3s ease-out, color 0.3s ease-out;
+
+    &:hover {
+      background-color: ${variant === "outline"
+        ? theme.colors.primary
+        : "transparent"};
+      color: ${variant === "outline"
+        ? theme.colors.light
+        : theme.colors.primary};
+      color: ${cta && `${theme.colors.light}`};
+    }
+  `;
+  if (type === "submit") {
+    return (
+      <button css={linkButtonStyles} type="submit">
+        {children}
+      </button>
+    );
+  }
+
+  return (
+    <Link href={href as string} passHref>
+      <a css={linkButtonStyles}>{children}</a>
+    </Link>
+  );
+};
+
+export default Button;
